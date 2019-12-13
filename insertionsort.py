35d2902e69e4082a937613fcec350e8b9755f373
sivareddy83 <58674884+sivareddy83@users.noreply.github.com>
2019-12-13 13:34:27 +0530

    insertion_sort_update_code_add_

--- insertionsort.py
+++ insertionsort.py
@@ -1,4 +1,19 @@
 
 def siva(list):
-    pass
+    for index in range(1,len(list)):
+        currentvalue=list[index]
+        position=index
+        while position>0 and list[position-1]> currentvalue:
+            list[position]=list[position-1]
+            position=position-1
+        currentvalue=position
+
+        
+
+list=[9,8,7,5,6,4,2,1,3]
+siva(list)
+print(list)
+
+        
+        
   
